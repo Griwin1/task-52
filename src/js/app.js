4ba1a9428738728a19eddbf1113ea2e51d2f85c0
Nikola Orlovski <nikikiko1@abv.bg>
2022-12-16 23:42:22 +0200

    Edit on dinamically element

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
 import "../scss/app.scss";
 
+let addArticle = () => {
+  const articleTag = document.createElement("article");
+  articleTag.classList.add("message");
+  articleTag.innerHTML = "LOREM IPUSUM";
+  document.body.appendChild(articleTag);
+}
+
+document.body.addEventListener("click", (event) => {
+    for (let i = 0; i < 5; i++) {
+      addArticle();
+    }
+    event.preventDefault();
+})
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
+  
 });
